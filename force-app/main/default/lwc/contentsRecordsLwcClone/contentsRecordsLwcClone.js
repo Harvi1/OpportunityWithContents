8dd5587d0aedf45600efccd662c8153d89c9f34d
Harvi1 <thakkarharvi17@gmail.com>
2023-02-04 18:05:46 +0530

    updated search functionality commit

--- force-app/main/default/lwc/contentsRecordsLwcClone/contentsRecordsLwcClone.js
+++ force-app/main/default/lwc/contentsRecordsLwcClone/contentsRecordsLwcClone.js
@@ -1,92 +1,97 @@
-import { LightningElement,api,track,wire } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import { subscribe, onError } from 'lightning/empApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { refreshApex } from '@salesforce/apex';
 import getcontactdata from '@salesforce/apex/OpportunityWithContents.getcontentdata';
 import updatecontentData from '@salesforce/apex/OpportunityWithContents.updatecontentData';
 import retrieveContent from '@salesforce/apex/ShowbasedonsearchContentStageName.retrieveContent';
 
-export default class ContentsRecordsLwc extends LightningElement {
+export default class ContentsRecordsLwcClone extends LightningElement {
     @api recordId;
     @track oppstage;
     @track message;
-    @track searchStageName;
     @track IsLoading = false;
     @track records;
-
-    handleSearchchange(event) {
-        this.searchStageName = event.target.value;
-    }
-
+    @track currentName;
+    @track search;
+    @track searchStageName;
+    @track dataNotFound;
+    
     subscription = {};
     @api channelName = '/event/Content_Event__e';
-
     @track Contentdata = [];
     @track updatedcontentdata = [];
     @track setContentdata = [];
-    @track opportunity =  [];
+    @track opportunity = [];
     error;
+    
     @track serachkey = '';
-
-    connectedCallback(){
-        console.log('recordId-->'+this.recordId);
+    connectedCallback() {
+        console.log('recordId-->' + this.recordId);
         this.registerErrorListener();
         this.handleSubscribe();
     }
-    /*handleSearchchange(event){
-        this.serachkey = event.target.value;
-        const searchString = event.target.value;
-        window.clearTimeout(this.delayTimeout);
-        this.delayTimeout = setTimeout(() => {
-            this.serachkey = searchString;
-     },2000);
-        console.log('this.serachkey-->>'+this.serachkey);
-    }*/
-
-    @wire(getcontactdata,{recordId :'$recordId'})
+    
+    @wire(getcontactdata, { recordId: '$recordId' })
     wiredContentData({ error, data }) {
         console.log('wire method called!!');
-            if (data) {
-                console.log('wire method called!!');
-                this.Contentdata = data;
-                this.IsLoading = false;
-                console.log('this.Contentdata-->>'+JSON.stringify(this.Contentdata));
-                this.serachkey = '';
-                this.error = undefined;
-            } else if (error) {
-                this.error = error;
-                this.Contentdata = undefined;
-            }     
-    } 
-    @wire(updatecontentData,{oppStage:'$oppstage'})
+        if (data) {
+            console.log('wire method called!!');
+            this.Contentdata = data;
+            this.IsLoading = false;
+            this.visibility = true;
+            this.isvisible = false;
+            console.log('this.Contentdata-->>' + JSON.stringify(this.Contentdata));
+            //this.serachkey = '';
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.Contentdata = undefined;
+        }
+    }
+    @wire(updatecontentData, { oppStage: '$oppstage' })
     updatedwiredContentData({ error, data }) {
         this.IsLoading = true;
-        console.log('this.IsLoading-->>'+this.IsLoading);
-            if (data) {
-                console.log('wire method called!!');
-                this.Contentdata = data;
-                this.IsLoading = false;
-                console.log('this.updatedcontentdata-->>'+JSON.stringify(this.Contentdata));
-                this.error = undefined;
-            } else if (error) {
-                this.error = error;
-                this.Contentdata = undefined;
-            }     
-    }
-     @wire(retrieveContent, { keySearch: '$searchStageName' })
-    wireRecord({ data, error }) {
+        console.log('this.IsLoading-->>' + this.IsLoading);
         if (data) {
-            this.records = data;
-            console.log('Check Data Here......' + this.records);
+            console.log('wire method called!!');
+            this.Contentdata = data;
+            this.IsLoading = false;
+            console.log('this.updatedcontentdata-->>' + JSON.stringify(this.Contentdata));
             this.error = undefined;
-            this.dataNotFound = '';
-            if (this.records == '') {
-                this.dataNotFound = 'There is no Content found';
-            }
-        } else {
+        } else if (error) {
             this.error = error;
-            this.data = undefined;
+            this.Contentdata = undefined;
         }
+    }
+    
+    // @wire(retrieveContent, { keySearch: '$searchStageName' })
+    // wireRecord({ data, error }) {
+    //     if (data) {
+    //         this.records = data;
+    //         this.isvisible = true;
+    //         this.visibility = false;
+    //         console.log('Check Data Here......' + this.records);
+    //         this.error = undefined;
+    //         this.dataNotFound = '';
+    //         if (this.records == '') {
+    //             this.dataNotFound = 'There is no Content found';
+    //         }
+    //     } else {
+    //         this.error = error;
+    //         this.data = undefined;
+    //     }
+    // }
+    //this.recorddata = this.records;
+    handleSearchchange(event) {
+        this.searchStageName = event.target.value;
+    }
+    handleSearchclick() {
+        retrieveContent({ keySearch: this.searchStageName })
+            .then(result => { 
+                this.records = result;
+            }).catch(error => {
+                console.error(error);
+            })
     }
     handleSubscribe() {
         console.log('handleSubscribe method called');
@@ -112,8 +117,8 @@ export default class ContentsRecordsLwc extends LightningElement {
             this.subscription = response;
         });
     }
-      //handle Error
-      registerErrorListener() {
+    //handle Error
+    registerErrorListener() {
         onError(error => {
             console.log('Received error from server: ', JSON.stringify(error));
         });
